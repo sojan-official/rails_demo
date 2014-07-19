cac927ad33ccd1f1ccb1da172c0404c48462b87d
Sojan Jose <sojan@pepalo.com>
2014-07-20 02:48:32 +0530

    patched the microposts mode code

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
- belongs_to :user
+class Micropost < ActiveRecord::Base
+  belongs_to :user
   default_scope -> { order('created_at DESC') }
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
@@ -10,3 +11,4 @@
     where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
           user_id: user.id)
   end
+end
